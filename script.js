cf7df3bbc821b09a7266280391e63bc7f5a9cd5f
Prakash <thenp26@gmail.com>
2025-08-27 21:14:57 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
   lucide.createIcons();
   function handleSubmit(event) {
       event.preventDefault();
@@ -23,3 +24,30 @@
   });
 
  
+=======
+  lucide.createIcons();
+  function handleSubmit(event) {
+      event.preventDefault();
+      const form = event.target;
+      const data = new FormData(form);
+      fetch(form.action, {
+        method: form.method,
+        body: data,
+        headers: { 'Accept': 'application/json' }
+      }).then(response => {
+        document.getElementById('form-status').textContent = response.ok ? 'Thanks for your message!' : 'Oops! Something went wrong.';
+        if (response.ok) form.reset();
+      }).catch(() => {
+        document.getElementById('form-status').textContent = 'Oops! Something went wrong.';
+      });
+    }
+     function closePopup() {
+    document.getElementById("whatsapp-popup").style.display = "none";
+  }
+
+  window.addEventListener("load", () => {
+    document.getElementById("whatsapp-popup").style.display = "block";
+  });
+
+ 
+>>>>>>> d0df90e (First commit from VS Code)
